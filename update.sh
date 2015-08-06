9fb093267c85a6869f4c5e56334a6c8699d13979
Stefan Müller <s.mueller@it.kls-glt.de>
2015-08-06 07:10:22 +0200

    Second file system layout
    From here: http://learnvimscriptthehardway.stevelosh.com/chapters/43.html

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Update the plugin from https://wiki.gnome.org/Projects/Vala/Vim
-wget https://wiki.gnome.org/Projects/Vala/Vim\?action\=AttachFile\&do\=get\&target\=vala.vim -O plugin/vala.vim
+wget https://wiki.gnome.org/Projects/Vala/Vim\?action\=AttachFile\&do\=get\&target\=vala.vim -O syntax/vala.vim
 
